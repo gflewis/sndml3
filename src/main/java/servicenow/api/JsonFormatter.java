04e8aac34564aec6908b3100eec41225439e9a81
gflewis <gflewis@MIT.EDU>
2018-02-28 16:38:03 -0500

    GSON JSON Formatter

--- src/main/java/servicenow/api/JsonFormatter.java
+++ src/main/java/servicenow/api/JsonFormatter.java
@@ -1,13 +1,18 @@
 package servicenow.api;
 
-import java.io.IOException;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
+//import java.io.IOException;
+//import com.fasterxml.jackson.databind.JsonNode;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 public class JsonFormatter {
 
+	/*
 	static ObjectMapper mapper;
 	static ObjectWriter prettyWriter;
 	
@@ -20,6 +25,15 @@ public class JsonFormatter {
 		JsonNode node = mapper.readTree(jsonText);
 		String result = prettyWriter.writeValueAsString(node);
 		return result;		
+	}
+	*/
+	
+	
+	public static String format(String jsonText) throws JsonSyntaxException {
+		JsonParser parser = new JsonParser();
+		JsonObject obj = parser.parse(jsonText).getAsJsonObject();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();	
+		return gson.toJson(obj);
 	}
 	
 }
