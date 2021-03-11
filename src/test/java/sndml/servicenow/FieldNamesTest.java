17472c0b366cdd181695dc8195f645b8f63a3926
gflewis <giles@gilesflewis.com>
2021-03-11 14:09:56 -0500

    FieldNamesTest

--- src/test/java/sndml/servicenow/FieldNamesTest.java
+++ src/test/java/sndml/servicenow/FieldNamesTest.java
@@ -32,4 +32,14 @@ public class FieldNamesTest {
 		assertFalse(f1.contains("delta"));
 	}
 	
+	@Test
+	public void testMultiLine() {
+		String s1 = "alpha\n beta\ngamma";
+		FieldNames f1 = new FieldNames(s1);
+		assertEquals(3, f1.size());
+		assertTrue(f1.contains("beta"));
+		assertTrue(f1.contains("alpha"));
+		assertFalse(f1.contains("delta"));
+	}
+	
 }
